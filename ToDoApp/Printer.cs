8d8a35ebbac0b8096e33741c1b27a28e94a5f4aa
r0nuh <cs.szilard@gmail.com>
2017-12-14 14:39:41 +0100

    refactor(todo-app): print usage with Printer class

--- ToDoApp/Printer.cs
+++ ToDoApp/Printer.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,7 +9,31 @@ namespace ToDoApp
 {
     class Printer
     {
-        
+        public string Content = File.ReadAllText(@"C:\Users\Szilard\Documents\greenfox\r0nuh-todo-app\ToDoApp\ToDo.txt");
+
+        public string[] CliArguments = {
+            "-l   Lists all the tasks",
+            "-a   Adds a new task",
+            "-r   Removes a task",
+            "-c   Completes a task" };
+
+        public void NoArgument()
+        {
+            Console.WriteLine("Command Line Todo application");
+            Console.WriteLine("=============================");
+            Console.WriteLine();
+            Console.WriteLine("Command line arguments:");
+            foreach (string argument in CliArguments)
+            {
+                Console.WriteLine(argument);
+            }
+        }
+
+
+        public void ListTasks()
+        {
+            Console.WriteLine(Content);
+        } 
     }
 }
 
--- ToDoApp/Program.cs
+++ ToDoApp/Program.cs
@@ -10,21 +10,11 @@ namespace ToDoApp
     {
         static void Main(string[] args)
         {
-            string[] cliArguments = {
-            "-l   Lists all the tasks",
-            "-a   Adds a new task",
-            "-r   Removes a task",
-            "-c   Completes a task" };
+            var print = new Printer();
+            print.NoArgument();
 
-            Console.WriteLine("Command Line Todo application");
-            Console.WriteLine("=============================");
-            Console.WriteLine();
-            Console.WriteLine("Command line arguments:");
-            foreach (string argument in cliArguments)
-            {
-                Console.WriteLine(argument);
-            }
-            Console.Read();
+            //Console.Read();
+            
         }
     }
 }
