eeba18345440dce07a2b5d5d51f8638d8464925b
r0nuh <cs.szilard@gmail.com>
2017-12-14 12:30:35 +0100

    feat(todo-app): print usage

---
+++ ToDoApp/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDoApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] cliArguments = {
+            "-l   Lists all the tasks",
+            "-a   Adds a new task",
+            "-r   Removes a task",
+            "-c   Completes a task" };
+
+            Console.WriteLine("Command Line Todo application");
+            Console.WriteLine("=============================");
+            Console.WriteLine();
+            Console.WriteLine("Command line arguments:");
+            foreach (string argument in cliArguments)
+            {
+                Console.WriteLine(argument);
+            }
+            Console.Read();
+        }
+    }
+}
